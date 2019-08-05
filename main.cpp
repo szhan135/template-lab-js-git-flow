3a65646f5ecdfa4d48d57263671ac375bfed97fb
Siyi Zhan <szhan135@ucr.edu>
2019-08-05 01:01:03 +0100

    Solution to the Program

---
+++ main.cpp
@@ -0,0 +1,14 @@
+
+for (var i=0; i<100; i++){
+		if(i%4==0 && i%6==0){
+			console.log("FizzBuzz");
+		}else if (i%4==0){
+			console.log("Fizz");
+		}else if (i%6==0){
+			console.log("Buzz")
+		}
+		else{
+			console.log(i);
+		}
+		}
+
